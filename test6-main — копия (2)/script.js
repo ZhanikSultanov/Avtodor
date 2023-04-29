3e1997cf636c28312a55c5f8b307d2af27f5415d
ZhanikSultanov <114715942+ZhanikSultanov@users.noreply.github.com>
2023-04-29 10:32:54 +0600

    Delete test6-main — копия (2) directory

--- test6-main — копия (2)/script.js
+++
@@ -1,9 +0,0 @@
-menu.onclick = function myFunction() {
-    var x = document.getElementById('muTopnav');
-    
-    if (x.className === "topnav"){
-        x.className += " responsive";
-    } else {
-        x.className ="topnav";
-    } 
-}
